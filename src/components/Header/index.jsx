0bc89a86b12b4e410e25f464e7823dbe1d1b5ba6
RafaTPz <camarodorato@gmail.com>
2024-04-15 17:11:41 -0300

    configurado

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export function Header() {
+    return (
+        <header>
+            <img src="https://static.vecteezy.com/system/resources/previews/000/626/835/original/green-leaf-logo-vector.jpg" alt="" width="80px" />
+        </header>
+    )
+}
