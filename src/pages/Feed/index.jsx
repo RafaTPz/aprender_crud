b6cffd149a618199fab99e850c15412a14e51668
RafaTPz <camarodorato@gmail.com>
2024-04-15 18:25:55 -0300

    aprendendo metodo get

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,10 +1,25 @@
-import { Card } from '../../components/Card'
-import './styles.css'
-
+import { Card } from "../../components/Card";
+import "./styles.css";
+import { useState, useEffect } from "react";
+import { api } from "../../lib/axios";
 export function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/posts")
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="feedContainer">
-      <Card />
+      {posts.map((post) => (
+        <Card key={post.id} post={post}/>
+      ))}
     </div>
   );
 }
